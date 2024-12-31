5318bbc27d2401e7b4ffa6ff4f654906716f4c8e
Srinidhi_Seethala <119839619+srinidhiseethala@users.noreply.github.com>
2024-12-31 14:37:49 +0530

    Uploading code: Primes in Range.py

---
+++ Primes in Range.py
@@ -0,0 +1,14 @@
+a=int(input())
+b=int(input())
+c=0
+def isprime(n):
+    if n<2:
+        return False
+    for i in range(2,n//2+1):
+        if n%i==0:
+            return False
+    return True
+for i in range(a,b+1):
+    if isprime(i):
+        c+=1
+print(c)
